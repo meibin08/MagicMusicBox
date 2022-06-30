76ce7401465b7151bf3e3411c87c37e92aa175f6
Bin <meibin08@163.com>
2022-06-30 14:05:08 +0800

    完善请求逻辑

--- src/components/playMark/play.tsx
+++ src/components/playMark/play.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { CaretRightOutlined } from '@ant-design/icons';
 import { clsPrefix } from '@/utils/serialization';
 import classnames from 'classnames';
+
 const prefixCls = 'playmark-component';
 const clsName = clsPrefix(prefixCls);
+
 const PlayMarkComponent = ({ className }: { className?: string }) => {
   return (
     <p className={classnames(prefixCls, { [`${className}`]: !!className })}>
